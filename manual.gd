4fee39e089d3ba018293a971ace6fbc54e5161bd
Dennis Payne <dulsi@identicalsoftware.com>
2021-09-11 21:19:16 -0400

    Start adding detail to manual. Added some mission text.

--- manual.gd
+++ manual.gd
@@ -5,8 +5,9 @@ extends RichTextLabel
 # var a = 2
 # var b = "text"
 var manual = {
-	"main" : "[center]Table of Contents[/center]\nSome text\n[url]for[/url]",
-	"for" : "[center]For statement[/center]\nFor description"
+	"main" : "[center]BASIC Manual[/center]\nTable of Contents\n[url]for[/url]\n[url]print[/url]",
+	"for" : "[center]For statement[/center]\nFor description",
+	"print" : "[center][b]PRINT statement[/b][/center]\n\nprint [<expression>] [[;|,]<expression>...]\n\nThe print commands outputs the arguments to the screen. The arguments may be seperated by a semicolon or comma. When specifying specific text to be printed it must be enclosed in double quotes. A comma between expressions will add a space to the output. If an argument's expression is invalid, an error message prints and execution aborts.\n\nA PRINT command moves to the beginning of the next line when finished.\n\nExamples\n\n10 print \"Hello World\"\n20 print 10 + 5\n30 b$ = \"Hello\"\n40 print b$,\"World\"\n\n[url=main]Return to Table of Contents[/url]"
 }
 
 # Called when the node enters the scene tree for the first time.
